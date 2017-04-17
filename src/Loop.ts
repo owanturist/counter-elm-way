145f89f7393ea8a8a1307ca237ef3d056a2d32ee
Anton Ovechkin <owanturist@gmail.com>
2017-04-17 15:22:10 +0700

    Switch type to interface

--- src/Loop.ts
+++ src/Loop.ts
@@ -17,12 +17,12 @@ export type Loop<Model, Msg> = [
 
 export type Update<Model> = <Msg extends Action>(msg: Msg, model: Model) => Loop<Model, Msg>;
 
-export type Store<Model> = {
-    dispatch: Dispatch<Model>,
-    getState(): Model,
-    subscribe(listener: () => void): Unsubscribe,
-    replaceReducer(nextReducer: Update<Model>): void
-};
+export interface Store<Model> {
+    dispatch: Dispatch<Model>;
+    getState(): Model;
+    subscribe(listener: () => void): Unsubscribe;
+    replaceReducer(nextReducer: Update<Model>): void;
+}
 
 export function createLoopStore<Model, Msg extends Action>(
     update: Update<Model>,
