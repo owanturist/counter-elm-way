16e74eec5970ca6fd477178c0e6b7ac892260534
Anton Ovechkin <owanturist@gmail.com>
2017-04-17 13:55:13 +0700

    Removed commented code

--- src/Counter/Cmd.ts
+++ src/Counter/Cmd.ts
@@ -6,16 +6,6 @@ import {
     increment
 } from './Types';
 
-// export const delayedIncrement = (delay: number): Promise<Msg> => new Promise((resolve) => {
-//     const timeoutID = setTimeout(() => {
-//         clearTimeout(timeoutID);
-
-//         resolve(
-//             increment()
-//         );
-//     }, delay);
-// });
-
 export const delayedIncrement = (delay: number) => Cmd.of(
     () => new Promise<Msg>((resolve) => {
         const timeoutID = setTimeout(() => {
