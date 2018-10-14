21b3f2752ea5be72db38a966e65ecc2273c81cad
Anton Ovechkin <owanturist@gmail.com>
2018-10-14 16:06:19 +0700

    Cmd.batch matches lenght of argument

--- src/Platform/Cmd.ts
+++ src/Platform/Cmd.ts
@@ -1,5 +1,19 @@
-export abstract class Cmd<M> {
-    public static batch<M>(cmds: Array<Cmd<M>>): Cmd<M> {
+export abstract class Cmd<T> {
+    public static batch<T>(cmds: Array<Cmd<T>>): Cmd<T> {
+        switch (cmds.length) {
+            case 0: {
+                return new None();
+            }
+
+            case 1: {
+                return cmds[ 0 ];
+            }
+
+            default: {
+                return new Batch(cmds);
+            }
+        }
+
         return new Batch(cmds);
     }
 
@@ -7,36 +21,59 @@ export abstract class Cmd<M> {
         return new None();
     }
 
-    protected static execute<M, R>(fn: (msg: M) => R, cmd: Cmd<M>): Promise<any> {
+    protected static execute<T, R>(fn: (msg: T) => R, cmd: Cmd<T>): Promise<R> {
         return cmd.execute(fn);
     }
 
-    protected static cons<M>(callPromise: () => Promise<M>): Cmd<M> {
+    protected static cons<T>(callPromise: () => Promise<T>): Cmd<T> {
         return new Single(callPromise);
     }
 
-    public abstract map<R>(fn: (msg: M) => R): Cmd<R>;
+    public abstract map<R>(fn: (msg: T) => R): Cmd<R>;
 
-    protected abstract execute<R>(fn: (msg: M) => R): Promise<R>;
+    protected abstract execute<R>(fn: (msg: T) => R): Promise<R>;
 }
 
-class Single<M> extends Cmd<M> {
-    constructor(private readonly callPromise: () => Promise<M>) {
+class Single<T> extends Cmd<T> {
+    constructor(private readonly callPromise: () => Promise<T>) {
         super();
     }
 
-    public map<R>(fn: (msg: M) => R): Cmd<R> {
-        return new Single(() => this.callPromise().then(fn));
+    public map<R>(fn: (msg: T) => R): Cmd<R> {
+        return new Map(fn, this);
     }
 
-    protected execute<R>(fn: (msg: M) => R): Promise<R> {
+    protected execute<R>(fn: (msg: T) => R): Promise<R> {
         return this.callPromise().then(fn);
     }
 }
 
-class None<M> extends Cmd<M> {
+class Map<T, R> extends Cmd<R> {
+    constructor(
+        private readonly fn: (msg: T) => R,
+        private readonly cmd: Cmd<T>
+    ) {
+        super();
+    }
+
+    public map<S>(fn: (msg: R) => S): Cmd<S> {
+        return new Map(
+            (msg: T): S => fn(this.fn(msg)),
+            this.cmd
+        );
+    }
+
+    protected execute<S>(fn: (msg: R) => S): Promise<S> {
+        return Cmd.execute(
+            (msg: T): S => fn(this.fn(msg)),
+            this.cmd
+        );
+    }
+}
+
+class None<T> extends Cmd<T> {
     public map<R>(): Cmd<R> {
-        return new None();
+        return this;
     }
 
     protected execute(): Promise<any> {
@@ -44,12 +81,12 @@ class None<M> extends Cmd<M> {
     }
 }
 
-class Batch<M> extends Cmd<M> {
-    constructor(private readonly cmds: Array<Cmd<M>>) {
+class Batch<T> extends Cmd<T> {
+    constructor(private readonly cmds: Array<Cmd<T>>) {
         super();
     }
 
-    public map<R>(fn: (msg: M) => R): Cmd<R> {
+    public map<R>(fn: (msg: T) => R): Cmd<R> {
         const result: Array<Cmd<R>> = [];
 
         for (const cmd of this.cmds) {
@@ -59,7 +96,7 @@ class Batch<M> extends Cmd<M> {
         return new Batch(result);
     }
 
-    protected execute<R>(fn: (msg: M) => R): Promise<any> {
+    protected execute<R>(fn: (msg: T) => R): Promise<any> {
         const result: Array<Promise<R>> = [];
 
         for (const cmd of this.cmds) {
