1ee45bf0b37fe99d63672d186ec7002e36f08bd5
Anton Ovechkin <owanturist@gmail.com>
2018-10-29 22:06:04 +0700

    Make Slider draggable

--- src/Changer.tsx
+++ src/Changer.tsx
@@ -19,14 +19,21 @@ import * as Utils from './Utils';
  * M O D E L
  */
 
+interface Dragging {
+    start: number;
+    delta: Maybe<number>;
+}
+
 export interface Model {
     currency: string;
+    dragging: Maybe<Dragging>;
 }
 
 export const isSame = (left: Model, right: Model): boolean => left.currency === right.currency;
 
 export const init = (currency: string): Model => ({
-    currency
+    currency,
+    dragging: Nothing
 });
 
 /**
@@ -36,12 +43,28 @@ export const init = (currency: string): Model => ({
 export type Msg
     = { $: 'CHANGE_CURRENCY'; _0: string }
     | { $: 'CHANGE_AMOUNT'; _0: Maybe<number> }
+    | { $: 'DRAG_START'; _0: number }
+    | { $: 'DRAGGING'; _0: number; _1: number }
+    | { $: 'DRAG_END' }
     ;
 
 export type Stage
     = { $: 'UPDATED'; _0: boolean; _1: Model }
     | { $: 'AMOUNT_CHANGED'; _0: Maybe<number> }
     ;
+
+
+const luft = (limit: number, delta: number): Maybe<number> => {
+    if (delta > 0) {
+        return delta - limit > 0 ? Just(delta - limit) : Nothing;
+    }
+
+    if (delta < 0) {
+        return delta + limit < 0 ? Just(delta + limit) : Nothing;
+    }
+
+    return Nothing;
+};
 
 export const update = (msg: Msg, model: Model): Stage => {
     switch (msg.$) {
@@ -60,6 +83,48 @@ export const update = (msg: Msg, model: Model): Stage => {
             return {
                 $: 'AMOUNT_CHANGED',
                 _0: msg._0
+            };
+        }
+
+        case 'DRAG_START': {
+            return {
+                $: 'UPDATED',
+                _0: false,
+                _1: {
+                    ...model,
+                    dragging: Just({
+                        start: msg._0,
+                        delta: Nothing
+                    })
+                }
+            };
+        }
+
+        case 'DRAGGING': {
+            return {
+                $: 'UPDATED',
+                _0: false,
+                _1: {
+                    ...model,
+                    dragging: Just({
+                        start: msg._0,
+                        delta: model.dragging.chain(dragging => dragging.delta).cata({
+                            Nothing: () => luft(50, msg._1 - msg._0),
+                            Just: () => Just(msg._1 - msg._0)
+                        })
+                    })
+                }
+            };
+        }
+
+        case 'DRAG_END': {
+            return {
+                $: 'UPDATED',
+                _0: false,
+                _1: {
+                    ...model,
+                    dragging: Nothing
+                }
             };
         }
     }
@@ -86,14 +151,44 @@ const stringToAmount = (input: string): Maybe<number> => {
 };
 
 const Root = styled.div`
+    flex-grow: 1;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
-    padding: 1em 2em;
+    padding: 1em 0;
     color: #fff;
+    user-select: none;
+    overflow-x: hidden;
 `;
 
-const Main = styled.label`
+interface CarouselProps {
+    shift?: number;
+}
+
+interface CarouselAttrs {
+    style: {
+        transform: string;
+    };
+}
+
+const Carousel = styled.div.attrs<CarouselProps, CarouselAttrs>({
+    style: props => ({
+        transform: `translateX(${props.shift || 0}px)`
+    })
+})`
+    flex: 1 0 auto;
+    display: flex;
+`;
+
+const Slide = styled.label`
+    box-sizing: border-box;
+    flex: 1 0 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0 2em;
+`;
+
+const Main = styled.div`
     display: flex;
     align-items: center;
     font-size: 2.4em;
@@ -102,10 +197,10 @@ const Main = styled.label`
 `;
 
 const Info = styled.div`
+    flex: 0 1 auto;
     display: flex;
     justify-content: space-between;
     margin-top: 1em;
-    font-size: .8em;
     font-weight: 300;
     opacity: .6;
 `;
@@ -119,6 +214,12 @@ const Input = styled.input`
     color: inherit;
     outline: none;
     text-align: right;
+
+    &::-webkit-inner-spin-button,
+    &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 `;
 
 const Small = styled.small`
@@ -127,7 +228,7 @@ const Small = styled.small`
 
 const Line = styled.ul`
     margin: .5em 0 0 -.1em;
-    padding: 0;
+    padding: 0 2em;
     list-style: none;
     text-align: center;
     font-size: 1.6em;
@@ -139,7 +240,7 @@ const Point = styled.li<{
 }>`
     display: inline-block;
     margin-left: .1em;
-    ${props => props.active ? null : `
+    ${props => props.active ? '' : `
         opacity: .5;
     `}
 
@@ -148,6 +249,61 @@ const Point = styled.li<{
     }
 `;
 
+interface DraggingMouseEvents<T> {
+    onMouseDown?(event: React.MouseEvent<T>): void;
+    onMouseMove?(event: React.MouseEvent<T>): void;
+    onMouseUp?(event: React.MouseEvent<T>): void;
+    onMouseLeave?(event: React.MouseEvent<T>): void;
+}
+
+function buildDraggingMouseEvents<T>(dispatch: Dispatch<Msg>, dragging: Maybe<Dragging>): DraggingMouseEvents<T> {
+    return dragging.cata<DraggingMouseEvents<T>>({
+        Nothing: () => ({
+            onMouseDown: event => dispatch({ $: 'DRAG_START', _0: event.screenX })
+        }),
+        Just: ({ start }) => ({
+            onMouseMove: event => dispatch({ $: 'DRAGGING', _0: start, _1: event.screenX }),
+            onMouseUp: () => dispatch({ $: 'DRAG_END' }),
+            onMouseLeave: () => dispatch({ $: 'DRAG_END' })
+        })
+    });
+}
+
+const ViewSlide: React.StatelessComponent<{
+    dispatch: Dispatch<Msg>;
+    amount: string;
+    currency: Currency;
+    donor: Maybe<Currency>;
+}> = ({ dispatch, currency, amount, donor }) => (
+    <Slide>
+        <Main>
+            {currency.code}
+
+            <Input
+                type="number"
+                value={amount}
+                step="0.01"
+                onChange={event => dispatch({
+                    $: 'CHANGE_AMOUNT',
+                    _0: stringToAmount(event.currentTarget.value)
+                })}
+            />
+        </Main>
+
+        <Info>
+            <span>You have <Small>{currency.symbol}</Small>{currency.amount}</span>
+
+            {donor.chain(donorCurrency => donorCurrency.convertTo(1, currency).map(rate => (
+                <span>
+                    <Small>{currency.symbol}</Small>1 =
+                    <Small>{donorCurrency.symbol}</Small>{Utils.round(2, rate)}
+                </span>
+            ))
+            ).getOrElse(<span></span>)}
+        </Info>
+    </Slide>
+);
+
 export const View: React.StatelessComponent<{
     dispatch: Dispatch<Msg>;
     model: Model;
@@ -155,43 +311,18 @@ export const View: React.StatelessComponent<{
     currencies: Array<Currency>;
     donor: Maybe<Currency>;
 }> = ({ dispatch, model, amount, currencies, donor }) => (
-    <Root>
-        <div></div>
-
+    <Root {...buildDraggingMouseEvents(dispatch, model.dragging)}>
         {Utils.find(currency => currency.code === model.currency, currencies).cata({
             Nothing: () => null,
             Just: (currency: Currency) => (
-                <div>
-                    <Main>
-                        {currency.code}
-
-                        <Input
-                            type="number"
-                            value={amount}
-                            onChange={event => dispatch({
-                                $: 'CHANGE_AMOUNT',
-                                _0: stringToAmount(event.currentTarget.value)
-                            })}
-                        />
-                    </Main>
-
-                    <Info>
-                        <span>You have <Small>{currency.symbol}</Small>{currency.amount}</span>
-
-                        {donor.chain(
-                            donorCurrency => donorCurrency
-                                .convertTo(1, currency)
-                                .map(rate => ([ donorCurrency.symbol, Utils.round(2, rate) ]))
-                        ).cata({
-                            Nothing: () => (<span></span>),
-                            Just: ([ symbol, rate ]) => (
-                                <span>
-                                    <Small>{currency.symbol}</Small>1 = <Small>{symbol}</Small>{rate}
-                                </span>
-                            )
-                        })}
-                    </Info>
-                </div>
+                <Carousel shift={model.dragging.chain(dragging => dragging.delta).getOrElse(0)}>
+                    <ViewSlide
+                        dispatch={dispatch}
+                        amount={amount}
+                        currency={currency}
+                        donor={donor}
+                    />
+                </Carousel>
             )
         })}
 
@@ -199,6 +330,7 @@ export const View: React.StatelessComponent<{
             {currencies.map(currency => (
                 <Point
                     active={currency.code === model.currency}
+                    onClick={() => dispatch({ $: 'CHANGE_CURRENCY', _0: currency.code })}
                     key={currency.code}
                 />
             ))}
--- src/Utils.ts
+++ src/Utils.ts
@@ -14,4 +14,21 @@ export const find = <T>(predicate: (value: T) => boolean, arr: Array<T>): Maybe<
     return Nothing;
 };
 
+export const filterMap = <T, R>(predicate: (value: T) => Maybe<R>, arr: Array<T>): Array<R> => {
+    const result: Array<R> = [];
+
+    for (const item of arr) {
+        predicate(item).cata({
+            Nothing: () => {
+                // do nothing
+            },
+            Just: (value: R) => {
+                result.push(value);
+            }
+        });
+    }
+
+    return result;
+};
+
 export const round = (fractionDigits: number, num: number): number => Number(num.toFixed(fractionDigits));
